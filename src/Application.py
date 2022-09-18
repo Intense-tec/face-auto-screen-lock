5fed89d01d0e71e30da73ec2252551eb22898431
brich.oussama <brich.oussama@gmail.com>
2022-09-18 21:51:41 +0200

    Finishing about window

--- src/Application.py
+++ src/Application.py
@@ -11,9 +11,10 @@ class Application(tk.Tk):
         self.vid = None
         self.photo = None
         self.canvas = None
+        self.bg_color = '#e6f3fc'
         self.title(title)
         self.geometry("900x600")
-        self.configure(bg='#e6f3fc')
+        self.configure(bg=self.bg_color)
         self.minsize(900, 600)
         self.maxsize(900, 600)
         self.add_button("Cancel", "app_quit", 780, 550, 100)
@@ -54,7 +55,30 @@ class Application(tk.Tk):
     def about_window(self):
         about_window = tk.Toplevel(self)
         about_window.title("About")
-        about_window.geometry("200x200")
+        about_window.geometry("400x200")
+        about_window.minsize(400, 200)
+        about_window.maxsize(400, 200)
+        about_window.configure(bg=self.bg_color)
+
+        editor_logo_img = Image.open("src/assets/img/editor_logo.png")
+        editor_logo_img = editor_logo_img.resize((220, 80))
+        editor_logo_tk = ImageTk.PhotoImage(editor_logo_img)
+        editor_logo_label = tk.Label(about_window, image=editor_logo_tk)
+        editor_logo_label.image = editor_logo_tk
+        editor_logo_label.place(relx=0.5, rely=0.25, anchor="center")
+
+        description_frame = tk.Frame(about_window)
+        description_frame.place(relx=0.5, rely=0.75, anchor="center")
+        description_label = tk.Label(description_frame,
+                                     bg='#fff', fg='#1e6aaf',
+                                     text="Auto Face Screen Lock V2.0 \n "
+                                          "Version published in 2022 \n"
+                                          "By TECOBRI - Open Source software \n ",
+                                     font='Arial 13 bold')
+        description_label.place(relx=0.5, rely=0.70, anchor="center")
+        description_label.pack()
+
+
 
 if __name__ == "__main__":
     app = Application("Face auto screen lock")
